f3e199e3a6e8f9f03f0f4619cdc7a1fd7f52ae0f
ye4241 <ye4241@gmail.com>
2020-01-16 11:00:57 +0800

    add dot-net-reactor package;

---
+++ dot-net-reactor/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.eziriz.com/download.php?download=dotnet_reactor_setup_6_2_0_0.exe' # download url, HTTPS preferred
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = '.NET Reactor'
+  checksum      = 'e4fc636d5235b4ab69374472664faee99bb15be68e102ea5f535675cf185f596'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs 
